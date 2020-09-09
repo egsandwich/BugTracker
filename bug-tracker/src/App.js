2f2a3a68f899320cab1f9c2c4042a2212cbeaa41
egsandwich <enahgvlanto@gmail.com>
2020-09-09 14:08:30 +0200

    better drawer, but need to fix grid again

--- bug-tracker/src/App.js
+++ bug-tracker/src/App.js
@@ -9,20 +9,79 @@ import Dashboard from "./components/Dashboard";
 import Project from "./components/Project";
 import {
   makeStyles, AppBar, Toolbar, Typography, Grid, Drawer, List,
-  ListItem, ListItemIcon, ListItemText, Link as LinkUI, Container
+  ListItem, ListItemIcon, ListItemText, Link as LinkUI, Container, useTheme, IconButton
 } from '@material-ui/core/';
 // import Divider from '@material-ui/core/Divider';
 import HomeIcon from '@material-ui/icons/Home'; //change to home icon
 // import AddIcon from '@material-ui/icons/Add'; //change to something else icon
 import { AddCircleOutlinedIcon as AddIcon } from '@material-ui/icons/AddCircleOutlined';
 import MenuIcon from '@material-ui/icons/Menu';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import clsx from 'clsx';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import Divider from '@material-ui/core/Divider'
+// ---------------------------------------------------------
 
+const drawerWidth = 240;
 
 const useStyles = makeStyles(theme => ({
   root: {
     textAlign: 'center',
-    width: '100%',
-    height: '100%',
+    // width: '100%',
+    // height: '100%',
+    display: 'flex',
+  },
+  appbar: {
+    transition: theme.transitions.create(['margin', 'width'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+  },
+  appBarShift: {
+    width: `calc(100% - ${drawerWidth}px)`,
+    marginLeft: drawerWidth,
+    transition: theme.transitions.create(['margin', 'width'], {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  hide: {
+    display: 'none',
+  },
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0,
+  },
+  drawerPaper: {
+    width: drawerWidth,
+  },
+  drawerHeader: {
+    display: 'flex',
+    alignItems: 'center',
+    padding: theme.spacing(0, 1),
+    // necessary for content to be below app bar
+    ...theme.mixins.toolbar,
+    justifyContent: 'flex-end',
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(3),
+    transition: theme.transitions.create('margin', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    marginLeft: -drawerWidth,
+  },
+  contentShift: {
+    transition: theme.transitions.create('margin', {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+    marginLeft: 0,
   },
   list: {
     width: 200,
@@ -49,8 +108,7 @@ const useStyles = makeStyles(theme => ({
 
 }));
 
-//dashboard
-//more views i guess
+
 function App() {
   //in use effect for the future, check if logged in
 
@@ -61,53 +119,33 @@ function App() {
 
 
   const classes = useStyles();
-  const [state, setState] = React.useState(false);
+  const theme = useTheme();
+  const [open, setOpen] = React.useState(false);
 
-  const toggleDrawer = (open) => (event) => {
-    if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
-      return;
-    }
-
-    setState(open);
+  const handleDrawerOpen = () => {
+    setOpen(true);
   };
 
-  const list = (
-    <Grid className={classes.gridList}>
-      <div
-        className={classes.list}
-        role="presentation"
-        onClick={toggleDrawer(false)}
-        onKeyDown={toggleDrawer(false)}
-      >
-
-
-        <List>
-          <Link to="/" style={{ textDecoration: 'none' }}>
-            <ListItem button>
-              <ListItemIcon><HomeIcon /></ListItemIcon>
-              <ListItemText primary="Home" />
-            </ListItem>
-          </Link>
-          <Link to="/registerProject" style={{ textDecoration: 'none' }}>
-            <ListItem button>
-              <ListItemIcon><AddIcon /></ListItemIcon>
-              <ListItemText primary="Add project" />
-            </ListItem>
-          </Link>
-        </List>
-      </div>
-    </Grid>
-  );
-
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
 
 
   return (
     <Router>
       <div className={classes.root}>
+        <CssBaseline />
         <Grid className={classes.grid}>
-          <AppBar position="static">
+          <AppBar position="fixed" className={clsx(classes.appBar, {
+            [classes.appBarShift]: open
+          })}>
             <Toolbar>
-              <MenuIcon onClick={toggleDrawer(true)} />
+              <IconButton onClick={handleDrawerOpen}
+                aria-label="open drawer"
+                edge="start"
+                className={clsx(classes.menuButton, open && classes.hide)}>
+                <MenuIcon />
+              </IconButton>
               <Typography variant="h3" className={classes.title}>
                 <Link to="/" style={{ textDecoration: 'none' }} >
                   <LinkUI className={classes.linkStyle} underline='none'>
@@ -120,9 +158,26 @@ function App() {
           </AppBar>
         </Grid>
 
-
-        <Drawer anchor='left' open={state} onClose={toggleDrawer(false)}>{list}</Drawer>
-        {/* <Drawer anchor='left' variant='permanent'>{list}</Drawer> */}
+        <Drawer className={classes.drawer}
+          variant="persistent"
+          anchor="left"
+          open={open}
+          close={{ paper: classes.drawerPaper, }}>
+          <div className={classes.drawerHeader}>
+            <IconButton onClick={handleDrawerClose}>
+              {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+            </IconButton>
+            <Divider />
+            <List>
+              <Link to="/registerProject">
+                <ListItem button>
+                  <ListItemIcon><HomeIcon /></ListItemIcon>
+                  <ListItemText primary="Home" />
+                </ListItem>
+              </Link>
+            </List>
+          </div>
+        </Drawer>
 
         {/* if logged in show dashboard */}
         <Switch>
@@ -136,13 +191,14 @@ function App() {
           <Route path="/" render={() => <div><h1>404</h1></div>} />
         </Switch>
         {/* bottom nav */}
+        {/* CssBaseline? */}
         {/* <footer className={classes.footer}>
           <Container maxWidth="sm">
             <Typography variant="body2">  {'Copyright © '} </Typography>
           </Container>
         </footer> */}
       </div>
-    </Router>
+    </Router >
   );
 }
 
