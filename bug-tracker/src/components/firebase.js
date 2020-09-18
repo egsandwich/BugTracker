51bb111972ddee67888fec0ffc8c8afafa78fe62
egsandwich <enahgvlanto@gmail.com>
2020-09-18 13:09:20 +0200

    export on firebase changed

--- bug-tracker/src/components/firebase.js
+++ bug-tracker/src/components/firebase.js
@@ -1,16 +1,4 @@
-// import * as firebase from 'firebase'
-// import app from 'firebase/app'
-// import 'firebase/auth'
-// import 'firebase/firestore'
-
-// import { firebase } from '@firebase/app';
-// import '@firebase/firestore'
-// import '@firebase/auth';
-
-import firebase from 'firebase'
-import 'firebase/firestore'
-import 'firebase/auth'
-
+import firebase from "firebase";
 
 const config = {
   apiKey: "AIzaSyAwpO3jAxS67_MCMRBQIdTMopk4rcegaJw",
@@ -21,39 +9,9 @@ const config = {
   messagingSenderId: "853256968072",
   appId: "1:853256968072:web:0d6bf5c4a223f045fb8c3d",
   measurementId: "G-HDERQJSZTM",
-}
-class Firebase {
-  constructor() {
-    firebase.initializeApp(config)
-    this.auth = firebase.auth()
-    this.db = firebase.firestore();
+};
 
-  }
-  login(email, password) {
-    return this.auth.signInWithEmailAndPassword(email, password)
-  }
+firebase.initializeApp(config);
 
-  logout() {
-    return this.auth.signOut()
-  }
 
-  async register(name, email, password) {
-    await this.auth.createUserWithEmailAndPassword(email, password)
-    return this.auth.currentUser.updateProfile({
-      displayName: name
-    })
-  }
-  isInitialized() {
-    return new Promise(resolve => {
-      this.auth.onAuthStateChanged(resolve)
-    })
-  }
-
-  getCurrentUsername() {
-    return this.auth.currentUser && this.auth.currentUser.displayName
-  }
-}
-
-// const db = firebaseApp.firestore();
-
-export default new Firebase();
+export default firebase;
