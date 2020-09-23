2cfc479f09ccaf903e89a8e537a1b753bcdc74dc
egsandwich <enahgvlanto@gmail.com>
2020-09-23 22:17:18 +0200

    changed export default on firebase config

--- bug-tracker/src/components/Signup.js
+++ bug-tracker/src/components/Signup.js
@@ -1,21 +1,20 @@
 import React, { useState, useCallback } from 'react'
-import firebase from './firebase'
+import base from './firebase'
 import { withRouter } from 'react-router-dom'
 
 
 function Signup(props) {
-    const [name, setName] = useState("");
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
-    const [persons, setPersons] = useState([{
-        name: "name1",
-        email: "email@email.com",
-    },]);
+    // const [persons, setPersons] = useState([{
+    //     name: "name1",
+    //     email: "email@email.com",
+    // },]);
 
     const onRegister = useCallback(async event => {
         event.preventDefault();
         try {
-            await firebase
+            await base
                 .auth()
                 .createUserWithEmailAndPassword(email, password)
             props.history.push('/login')
@@ -30,20 +29,13 @@ function Signup(props) {
     return (
         <div>
             <form>
-                <p> <label>Name:</label> </p>
-                <p> <input value={name} onChange={(event) => setName(event.target.value)} />  </p>
                 <p> <label>Email:</label> </p>
                 <p> <input value={email} onChange={(event) => setEmail(event.target.value)} />  </p>
                 <p> <label>Password:</label> </p>
                 <p> <input type="password" value={password} onChange={(event) => setPassword(event.target.value)} />  </p>
                 <p><button type="submit" onClick={onRegister}>Sign up</button></p>
             </form>
-            {/* {persons.map((person) => (
-                <div>
-                    <p>name: {person.name}</p>
-                    <p>email: {person.email}</p>
-                </div>
-            ))} */}
+
         </div>
     )
 }
