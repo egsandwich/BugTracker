2d13230796054c632ade1ba0273350109ecfa1df
egsandwich <enahgvlanto@gmail.com>
2020-10-06 20:51:25 +0200

    setting up

--- bug-tracker/src/components/Dashboard.js
+++ bug-tracker/src/components/Dashboard.js
@@ -3,12 +3,15 @@ import Preview from "./Preview";
 import NavBar from "./NavBar";
 import Header from "./Header";
 import base from "./firebase";
+import firebase from 'firebase'
 import { Grid, Typography, Paper, Card, Button, Link as LinkUI, Box } from '@material-ui/core'
 import { makeStyles, useTheme } from '@material-ui/core/styles'
 // import AddCircleOutlineTwoToneIcon from '@material-ui/icons/AddCircleOutlineTwoTone';
 import AddCircleOutlinedIcon from '@material-ui/icons/AddCircleOutlined';
 import { BrowserRouter as Router, Switch, Route, Link, withRouter } from 'react-router-dom';
 import ChartPriority from "./ChartPriority";
+import ChartStatus from "./ChartStatus";
+import ChartType from "./ChartType";
 import { AuthContext } from "./Auth";
 
 function Dashboard(props) {
@@ -18,13 +21,13 @@ function Dashboard(props) {
 
     useEffect(() => {
         if (currentUser != null) {
-            if (currentUser.displayName != null) {
-                base.firestore().collection('users').where('userId', '==', currentUser.uid)
-                    .get().then(snapshot => snapshot.docs.forEach(doc => {
+            if (currentUser.displayName == null) {
+                base.firestore().collection('users').doc(currentUser.uid)
+                    .get().then(doc => {
                         currentUser.updateProfile({
                             displayName: doc.data().firstName + " " + doc.data().lastName
                         })
-                    }))
+                    })
             }
         }
     }, [])
@@ -40,6 +43,8 @@ function Dashboard(props) {
         < Box >
             <Header username={currentUser.displayName} />
             <ChartPriority />
+            <ChartStatus />
+            <ChartType />
             <div>
                 <button onClick={logout}>Logout</button>
             </div>
