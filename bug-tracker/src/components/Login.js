109b53322f40af06fd5975c95e2d8ad146d74be4
egsandwich <enahgvlanto@gmail.com>
2020-10-28 20:50:57 +0100

    ok login?

--- bug-tracker/src/components/Login.js
+++ bug-tracker/src/components/Login.js
@@ -1,5 +1,6 @@
 import React, { useState, useCallback } from "react";
 import { withRouter } from 'react-router-dom'
+import { Grid, Typography, Paper, Card, Button, Input, Box,FormControl } from '@material-ui/core'
 import base from './firebase'
 
 function Login(props) {
@@ -28,14 +29,24 @@ function Login(props) {
 
     return (
         <div>
-            <h1>Login</h1>
-            <form onSubmit={login}>
-                <p> <label>Email:</label> </p>
-                <p> <input value={email} onChange={(event) => setEmail(event.target.value)} />  </p>
-                <p> <label>Password:</label> </p>
-                <p> <input type="password" value={password} onChange={(event) => setPassword(event.target.value)} />  </p>
-                <p><button disabled={email.length === 0 || password.length === 0}type="submit">Login</button></p>
-            </form>
+            <Box m={2}>
+            <Grid container layout={'row'} spacing={3}>
+                <Grid item xl={12} xs={12}>
+                    <Typography variant="h4">Log In</Typography>
+                </Grid>
+                <Box m={1}>
+                    <form onSubmit={login}>
+                        <Grid item xs={12}>
+                        <Input placeholder="Email" value={email} onChange={(event) => setEmail(event.target.value)} />  
+                        </Grid>
+                        <Grid item>
+                        <p> <Input placeholder="Password" type="password" value={password} onChange={(event) => setPassword(event.target.value)} />  </p>
+                        </Grid>
+                        <p><Button variant="contained" color="primary" disabled={email.length === 0 || password.length === 0}type="submit">Login</Button></p>
+                    </form>
+                </Box>
+            </Grid>
+            </Box>
         </div >
     );
 }
