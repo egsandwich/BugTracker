e8513dafd53b3fbf15ef4fe9695e7ac331c6575d
egsandwich <enahgvlanto@gmail.com>
2020-11-02 14:30:20 +0100

    better logic ?

--- bug-tracker/src/components/CreateProject.js
+++ bug-tracker/src/components/CreateProject.js
@@ -1,15 +1,14 @@
 import React, { useState, useContext, useEffect } from "react";
 import base from "./firebase";
 import firebase, { firestore } from 'firebase';
-import { Redirect, withRouter, Route } from "react-router-dom";
+import { withRouter } from "react-router-dom";
 import { makeStyles } from '@material-ui/core/styles';
-import FilledInput from '@material-ui/core/FilledInput';
+import Box from '@material-ui/core/Box';
 import FormControl from '@material-ui/core/FormControl';
-import FormHelperText from '@material-ui/core/FormHelperText';
 import Input from '@material-ui/core/Input';
+import Checkbox from '@material-ui/core/Checkbox';
 import InputLabel from '@material-ui/core/InputLabel';
 import Button from '@material-ui/core/Button';
-import OutlinedInput from '@material-ui/core/OutlinedInput';
 import Typography from '@material-ui/core/Typography';
 import Grid from '@material-ui/core/Grid';
 import { AuthContext } from "./Auth";
@@ -29,8 +28,9 @@ const useStyles = makeStyles((theme) => ({
 function CreateProject(props) {
     const { currentUser } = useContext(AuthContext)
     const [projectName, setProjectName] = useState("");
-    const [users, setUsers] = useState([""]);
-    const [projectMembers, setProjectMembers] = useState([null]);
+    const [users, setUsers] = useState([]);
+    const [projectMembers, setProjectMembers] = useState([]);
+    const [allCheckDefault, setAllCheckDefault] = useState(false);
     
     const db = base.firestore();
 
@@ -42,14 +42,15 @@ function CreateProject(props) {
                 email: doc.data().email,
                 firstName: doc.data().firstName,
                 lastName: doc.data().lastName,
+                select: false,
             })))
         })
 
-    }, [db.collection('users')])
+    }, [])
 
     useEffect(() => {
-        console.log(users)
-    }, [users.length > 0])
+    console.log(projectMembers)
+    }, [projectMembers])
 
     const createProject = (event) => {
         event.preventDefault();
@@ -60,21 +61,30 @@ function CreateProject(props) {
         }).then((docRef) => {
             try{
             db.collection('users').doc(currentUser.uid)
-            .collection('myProjects').add({
+            .collection('myProjects').doc(docRef.id).set({
                 projectId: docRef.id,
                 projectName, projectName,
                 owner: true,
             })
-            projectMembers.filter(nullChecker).map(member => {
-                db.collection('users').doc(member)
-                .collection('myProjects').add(
-                    {projectId: docRef.id,
+            projectMembers.map(member => {
+                db.collection('users').doc(member.userId)
+                .collection('myProjects').doc(docRef.id).set(
+                    {
+                    projectId: docRef.id,
                     projectName: projectName, 
                     owner: false,
+                    userOwner: currentUser.uid,
                 }
                 )})
                 setProjectName("");
-                setProjectMembers([""]);
+                setProjectMembers([]);
+                setUsers(users.map(u => {
+                    if(u.select){
+                        u.select = !u.select;
+                    }
+                    return u;
+                }))
+                setAllCheckDefault(false);
                 props.history.push('/myProjects')
                 } 
                 catch (error) {
@@ -87,72 +97,95 @@ function CreateProject(props) {
          
         })
     };
-
-
+    const addMemberHandler = (event) => {
+        users.map(user => {
+            if(user.select){
+                setProjectMembers(projectMembers => [... projectMembers, {
+                    userId: user.userId,
+                    firstName: user.firstName,
+                    lastName: user.lastName,
+                }])
+                
+            }
+        })
+    }
 
    
 
-    function nullChecker(value){
-        return value != null
-    }
 
-    const handleCheckedValue = (event) => {
-        event.preventDefault()
-        if(!projectMembers.includes(event.target.value))
-            setProjectMembers([... projectMembers, event.target.value])
-        else
-            alert("already included")
-    }
     const classes = useStyles();
     return (
         <div>
-            Create project
-            <form>
-                <label>Project name</label>
-                <input value={projectName} onChange={(event) => setProjectName(event.target.value)} />
-                <p><label>Add members</label> </p>
-                {users.map(user =>(
-                    <p key={user.userId}><button onClick={handleCheckedValue} value={user.userId}> Add </button> {user.firstName} {user.lastName} </p>
-                ))}
-                {/* fix this */}
-                {projectMembers.map(members => (
-                    <p key={members}>{members}</p>
-                ))}
-                <button onClick={createProject} disabled={projectName.length < 1}>Create project</button>
-
-            </form>
-            {/* <Grid>
-                <Grid container>
-                    <Typography variant="h4">Add project </Typography>
-                </Grid>
-                <form className={classes.root}>
-                    <Grid container>
+            <Box m={2} flexGrow={1}>
+                <Grid container spacing={3}>
+                <Grid item xs={12} md={6}>
+                    <Typography variant="h4">Add project</Typography>
+                    <form>
+                    <Grid item xs={12} md={6}>
                         <FormControl>
                             <InputLabel>Project name</InputLabel>
-                            <Input id="component-simple"
+                            <Input required
                                 value={projectName}
                                 onChange={(event) => setProjectName(event.target.value)}
                             />
                         </FormControl>
                     </Grid>
-                    <Grid container>
-                        <FormControl>
-                            <InputLabel>Project Owner</InputLabel>
-                            <Input id="component-simple"
-                                value={projectOwner}
-                                onChange={(event) => setProjectOwner(event.target.value)}
-                            />
-                        </FormControl>
-                    </Grid>
-                    <Grid container>
-                        <Button variant="contained" type="submit" onClick={createProject}>
-                            Create Project
-        </Button>
-                    </Grid>
+                    </form>
+                </Grid>
 
-                </form>
-            </Grid>
-            */}
+                <Grid item xs={12} md={6}>
+                    <Typography variant="h5">Add members</Typography>
+                        <table>
+                            <thead>
+                                <tr>
+                                <th scope="col">
+                                    <Checkbox checked={allCheckDefault} onChange={(event) => {
+                                        setUsers(users.map(user => {
+                                            user.select = event.target.checked; 
+                                            return user;
+                                        }))
+                                        setAllCheckDefault(!allCheckDefault);
+                                    }}/>
+                                </th>
+                                <th scope="col">First</th>
+                                <th scope="col">Last</th>
+                                <th scope="col">Email</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                            {users.map(user =>(
+                                <tr>
+                                    <th scope="col">
+                                        <Checkbox checked={user.select} onChange={(event) => {
+                                            setUsers(users.map(u => {
+                                                let checked = event.target.checked;
+                                                if(user.userId === u.userId){
+                                                u.select = checked;
+                                                }
+                                                return u;
+                                            }))
+                                        }}/>
+                                    </th>
+                                    <th scope="col">{user.firstName}</th>
+                                    <th scope="col">{user.lastName}</th>
+                                    <th scope="col">{user.email}</th>
+                                </tr>
+                    ))}
+                            </tbody>
+                        </table>
+                        <Button variant="contained" size="small" onClick={addMemberHandler}>Add members</Button>
+                    </Grid>
+              
+                    <br/>
+                    
+                        <Grid item xs={12}>
+                            <Button onClick={createProject} variant="contained" type="submit" disabled={projectName.length < 1}>
+                                Create Project
+                            </Button>
+                        </Grid>
+
+                </Grid>
+           </Box>
         </div >
     );
 }
