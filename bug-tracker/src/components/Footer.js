7063b271d49a6b2db344e492b81576801b76baf8
egsandwich <enahgvlanto@gmail.com>
2020-11-02 14:25:40 +0100

    still gives errors and need to fix position

---
+++ bug-tracker/src/components/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import {Paper, Tabs, Tab} from '@material-ui/core'
+
+function Footer() {
+    return (
+        <Paper>
+            <Tabs centered>
+                <Tab label="Copyright 2020"/>
+                <Tab label="Github"/>
+                
+            </Tabs>
+        </Paper>
+    )
+}
+
+export default Footer
