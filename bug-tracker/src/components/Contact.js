2a9620bdfc496c83b1533cace50138bd6c1fe637
egsandwich <enahgvlanto@gmail.com>
2020-12-10 14:21:55 +0100

    added icons

--- bug-tracker/src/components/Contact.js
+++ bug-tracker/src/components/Contact.js
@@ -1,11 +1,29 @@
 import React from "react";
+import { Link, Redirect } from 'react-router-dom'
+import GitHubIcon from '@material-ui/icons/GitHub'
+import { Box, Typography, IconButton } from '@material-ui/core';
 
-function Contact(props) {
+
+function Contact() {
+
+    const goToGit = () => {
+        window.open("https://github.com/egsandwich/BugTracker")
+    }
+
     return (
-
-        <div>
-            <h1>Contact</h1>
-        </div>
+        <Box m={3}>
+            <Typography variant="h5">
+                Contact us
+            </Typography>
+            <Typography variant="h6">
+                If you find any bugs, please report it
+            </Typography>
+            <Box>
+                <IconButton onClick={goToGit}>
+                    <GitHubIcon />
+                </IconButton>
+            </Box>
+        </Box>
     );
 }
 
--- bug-tracker/src/components/Footer.js
+++ bug-tracker/src/components/Footer.js
@@ -1,13 +1,16 @@
 import React from 'react'
-import {Paper, Tabs, Tab} from '@material-ui/core'
+import { Paper, Tabs, Tab } from '@material-ui/core'
+import CopyrightIcon from '@material-ui/icons/Copyright'
 
 function Footer() {
     return (
         <Paper>
             <Tabs centered>
-                <Tab label="Copyright 2020"/>
-                <Tab label="Github"/>
-                
+                <Tab icon={<CopyrightIcon fontSize="small" />} disabled label="Copyright 2020" />
+
+                {/* <Tab icon={<GitHubIcon fontSize="small" />} label="Github"
+                    containerElement= /> */}
+
             </Tabs>
         </Paper>
     )
