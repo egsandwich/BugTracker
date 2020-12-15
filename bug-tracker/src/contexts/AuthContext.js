8eb7a375649dc40f239244f4df53c2273cc6a801
egsandwich <enahgvlanto@gmail.com>
2020-12-15 14:53:58 +0100

    added methods for editing profile

--- bug-tracker/src/contexts/AuthContext.js
+++ bug-tracker/src/contexts/AuthContext.js
@@ -1,28 +1,53 @@
 import React, { useContext, useEffect, useState } from 'react'
 import base from '../firebase'
+import firebase from 'firebase'
 
 const AuthContext = React.createContext();
 
-export function useAuth(){
+export function useAuth() {
     return useContext(AuthContext);
 }
-export function AuthProvider({children}){
+export function AuthProvider({ children }) {
     const [currentUser, setCurrentUser] = useState()
     const [loading, setLoading] = useState(true)
 
-    function signup(email, password){
+    function signup(email, password) {
         return base.auth().createUserWithEmailAndPassword(email, password)
     }
 
-    function login(email, password){
+    function login(email, password) {
         return base.auth().signInWithEmailAndPassword(email, password)
     }
 
-    function logout(){
+    function logout() {
         return base.auth().signOut()
     }
 
-    useEffect(()=> {
+    function deleteProfile() {
+        return currentUser.delete();
+    }
+
+    function editEmail(email) {
+        return currentUser.updateEmail(email);
+    }
+
+    function reauthenticate(password) {
+        var cred = firebase.auth.EmailAuthProvider.credential(currentUser.email, password)
+        return currentUser.reauthenticateWithCredential(cred);
+    }
+
+    function editDisplayName(firstName, lastName) {
+        return currentUser.updateProfile({
+            displayName: firstName + " " + lastName,
+        })
+
+    }
+
+    function editPassword(password) {
+        return currentUser.updatePassword(password);
+    }
+
+    useEffect(() => {
         const unsubscribe = base.auth().onAuthStateChanged(user => {
             setCurrentUser(user)
             setLoading(false)
@@ -35,7 +60,12 @@ export function AuthProvider({children}){
         currentUser,
         signup,
         login,
-        logout
+        logout,
+        deleteProfile,
+        editEmail,
+        editDisplayName,
+        editPassword,
+        reauthenticate
     }
     return (
         <AuthContext.Provider value={value}>
